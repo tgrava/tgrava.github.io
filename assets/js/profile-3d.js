fb8407b98cc6f76cbecf75dd75af72a270b26dab
Thomas <thomasugrava@gmail.com>
2025-04-26 19:32:14 +0200

    smoke V15

--- assets/js/profile-3d.js
+++ assets/js/profile-3d.js
@@ -76,7 +76,7 @@ function initAvatar() {
       // 7. Create smoke mesh and attach to model
       const smokeMesh = createSmokeMesh(radius);
       // your new Blender coordinates (relative to model origin):
-      smokeMesh.position.set(0.25129, 0.034452, -0.07226);
+      smokeMesh.position.set(0.25129, 0.034452, -0.02226);
       model.add(smokeMesh);
 
       // 8. Render loop with continuous smoke and auto-rotate
