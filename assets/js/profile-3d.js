e9d65ae1206517f633a4ecda0fbdcc9fe9c01604
Thomas <thomasugrava@gmail.com>
2025-04-26 19:42:31 +0200

    smoke V21

--- assets/js/profile-3d.js
+++ assets/js/profile-3d.js
@@ -78,7 +78,7 @@ function initAvatar() {
       // Your Blender coords; invert Y/Z if needed after flipping the model
       smokeMesh.position.set(
         0.25129,    // X
-        0.0,   // Y
+        0.154452,   // Y
         0.07226    // Z
       );
       model.add(smokeMesh);
