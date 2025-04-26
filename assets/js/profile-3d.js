3c86f0b1c3789a7525712913f31c4b5ee6e85504
Thomas <thomasugrava@gmail.com>
2025-04-26 19:44:18 +0200

    smoke V22

--- assets/js/profile-3d.js
+++ assets/js/profile-3d.js
@@ -79,7 +79,7 @@ function initAvatar() {
       smokeMesh.position.set(
         0.25129,    // X
         0.154452,   // Y
-        0.07226    // Z
+        0.17226    // Z
       );
       model.add(smokeMesh);
 
