db8966a2644c847d33756825cf62387e68feb7ff
Thomas <thomasugrava@gmail.com>
2025-04-26 20:52:37 +0200

    smoke V33

--- assets/js/profile-3d.js
+++ assets/js/profile-3d.js
@@ -23,7 +23,7 @@ function initAvatar() {
   // 2. Scene & Camera
   const scene  = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 100);
-  camera.position.set(0, 1, -1000);
+  camera.position.set(0, 1000, -100);
 
   // 3. Lights
   scene.add(new THREE.AmbientLight(0xffffff, 1.5));
