09346bdbfe236abab3645fd5be7f4876046d82a5
Thomas <thomasugrava@gmail.com>
2025-04-26 21:40:16 +0200

    smoke V44

--- assets/js/profile-3d.js
+++ assets/js/profile-3d.js
@@ -39,7 +39,7 @@ function initAvatar() {
   function resize() {
     const w = canvas.clientWidth, h = canvas.clientHeight;
     if (canvas.width !== w || canvas.height !== h) {
-      renderer.setSize(w, h, true);
+      renderer.setSize(w, h, false);
       camera.aspect = w / h;
       camera.updateProjectionMatrix();
     }
